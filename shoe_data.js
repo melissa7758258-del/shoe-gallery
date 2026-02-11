95e4e1d7ac0a9f216bbdc9228ef49bc2ada2ddb7
melissa7758258-del <melissa7758258@gmail.com>
2026-02-11 16:23:33 +0800

    Add files via upload

---
+++ shoe_data.js
@@ -0,0 +1,59 @@
+// 所有鞋子的信息数据，按品类分类，后续只需添加/修改这里
+const shoeList = [
+  {
+    img: "images/拖鞋_01.jpg",    // 图片路径
+    name: "纯色拖鞋",         // 鞋子名称
+    category: "拖鞋",            // 品类（必须和导航栏一致：男鞋/女鞋/运动鞋/拖鞋）
+    price: "¥10",               // 价格
+    size: "39-44"                // 尺码
+  },
+  {
+    img: "images/拖鞋_02.jpg",
+    name: "碎花人字拖",
+    category: "拖鞋",
+    price: "¥10",
+    size: "35-40"
+  },
+  {
+    img: "images/拖鞋_03.jpg",
+    name: "晕染人字拖",
+    category: "拖鞋",
+    price: "¥10",
+    size: "38-45"
+  },
+  {
+    img: "images/拖鞋_04.jpg",
+    name: "纯色人字拖",
+    category: "拖鞋",
+    price: "¥39",
+    size: "36-45"
+  },
+  {
+    img: "images/拖鞋_05.jpg",
+    name: "迷彩拖鞋",
+    category: "拖鞋",
+    price: "¥10",
+    size: "35-40"
+  }, 
+   {
+    img: "images/拖鞋_06.jpg",
+    name: "带钻洞洞鞋",
+    category: "拖鞋",
+    price: "¥39",
+    size: "36-45"
+  },
+   {
+    img: "images/拖鞋_07.jpg",
+    name: "皮质凉拖",
+    category: "拖鞋",
+    price: "¥45",
+    size: "35-40"
+  },
+    {
+    img: "images/拖鞋_08.jpg",
+    name: "纯色洞洞鞋",
+    category: "拖鞋",
+    price: "¥33",
+    size: "36-45"
+  }, // 复制上面的{}块，添加更多鞋子信息，100多款都按这个格式加
+];
